bea907e34d6affe24c9c43d87ef1b511e813feef
Massinissa Djellouli <massinissadjellouli@gmail.com>
2023-02-09 22:25:43 -0500

    added form control and error in logincomponent

--- synthese-frontend/src/app/components/home/login/login.component.ts
+++ synthese-frontend/src/app/components/home/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { LoginService } from '../../../services/login/login.service';
-
+import { FormControl } from '@angular/forms';
+import { Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -8,9 +9,30 @@ import { LoginService } from '../../../services/login/login.service';
 })
 
 export class LoginComponent {
+  username = new FormControl('',[Validators.required,Validators.minLength(6),Validators.maxLength(12)]);
+  password = new FormControl('',[Validators.required,Validators.minLength(8)]);
   constructor(private loginService:LoginService){
 
   }
-
-  login = this.loginService.login
+  validate = () => {
+    if(!this.username.valid){
+      this.fieldError = "Ce matricule n'est pas valide. (Entre 6-12 chars)"
+      return false;
+    }
+    if(!this.password.valid){
+      this.fieldError = "Ce mot de passe est invalide. (Min 8 chars)"
+      return false;
+    }
+    this.fieldError = "";
+    return true;
+  }
+  getLoginType = () => this.loginService.loginType as string;
+  login = () => {
+    if(!this.validate()){
+      return;
+    }
+    this.loginService.login({username:this.username.value!,password:this.password.value!})
+  }
+  fieldError = "";
+  getError = () => {return this.fieldError == "" ? this.loginService.error:this.fieldError};
 }
