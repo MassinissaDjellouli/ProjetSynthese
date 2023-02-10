e4feef882bb73223fd43a181312d0d856e705932
Massinissa Djellouli <massinissadjellouli@gmail.com>
2023-02-09 22:26:52 -0500

    added request handling for login

--- synthese-frontend/src/app/services/login/login.service.ts
+++ synthese-frontend/src/app/services/login/login.service.ts
@@ -1,24 +1,31 @@
 import { Injectable } from '@angular/core';
 import { LoginType } from '../../interfaces/LoginType';
+import { isError, parseError, RequestService } from '../request/request.service';
+import { ApiError } from '../../interfaces/ApiError';
+import { ApiResponse } from '../../interfaces/ApiResponse';
+import { Credentials } from 'src/app/interfaces/Credentials';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { 
+  constructor(private reqService:RequestService) { 
     this.loginType = "Étudiant"
   }
-
+  error:string = ""
   loginType:LoginType;
   changeLoginType = (newType:LoginType) => { this.loginType = newType }   
-  login = async ():Promise<boolean> => {
+  login = async (credentials:Credentials):Promise<boolean> => {
     let loginFunc = this.getLoginFunction();
-    let result = await loginFunc();
-
+    let result:ApiError|ApiResponse = await loginFunc(credentials);
+    if(isError(result)){
+      this.error = parseError(result);
+      return false;
+    }
     return true;
   }
-  private getLoginFunction = () => {
+  private getLoginFunction = ():Function => {
     switch(this.loginType) {
       case "Étudiant":
         return this.studentLogin;
@@ -26,22 +33,28 @@ export class LoginService {
         return this.teacherLogin;
       case "Administrateur":
         return this.adminLogin;
+      case "Gestionnaire":
+        return this.managerLogin;
       default:
         throw new Error("Non-existant user type:" + this.loginType);
     }
   }
 
-  private adminLogin = async () => {
+  private managerLogin = async (credentials:Credentials) => {
+    this.reqService.getRequest("manager/login")
+  }
+
+  private adminLogin = async (credentials:Credentials) => {
     console.log("admin");
     
   }
 
-  private studentLogin = async () => {
+  private studentLogin = async (credentials:Credentials) => {
     console.log("stud");
 
   }
 
-  private teacherLogin = async () => {
+  private teacherLogin = async (credentials:Credentials) => {
     console.log("teach");
 
   }
