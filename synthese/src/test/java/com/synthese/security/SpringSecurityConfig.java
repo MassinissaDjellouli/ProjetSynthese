1e759bb02ea8d05444a71fb5687adf5b25b2b1b9
Massinissa Djellouli <massinissadjellouli@gmail.com>
2023-02-09 22:45:14 -0500

    Added spring security filter chain

--- synthese/src/test/java/com/synthese/security/SpringSecurityConfig.java
+++ synthese/src/test/java/com/synthese/security/SpringSecurityConfig.java
@@ -1,16 +1,27 @@
 package com.synthese.security;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
-@EnableGlobalMethodSecurity(prePostEnabled = true,securedEnabled = true,jsr250Enabled = true)
+@EnableMethodSecurity(securedEnabled = true,jsr250Enabled = true)
 @EnableWebSecurity
 public class SpringSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.csrf().disable().authorizeRequests().antMatchers().anyRequest().permitAll().and().build();
+        return http.csrf().disable().authorizeHttpRequests()
+                .requestMatchers(HttpMethod.DELETE).hasRole("ADMIN")
+                .requestMatchers("/api/student/**").hasRole("STUDENT")
+                .requestMatchers("/api/teacher/**").hasRole("TEACHER")
+                .requestMatchers("/api/admin/**").hasRole("ADMIN")
+                .requestMatchers("/api/manager/**").hasRole("MANAGER")
+                .anyRequest().authenticated()
+                .and().httpBasic()
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().build();
     }
 }
