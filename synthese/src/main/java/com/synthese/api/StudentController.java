c2a453cc6d85820b02a1d2a5a671a3334efdd814
Massinissa Djellouli <massinissadjellouli@gmail.com>
2023-02-09 22:34:08 -0500

    Added login endpoint in StudentController

--- synthese/src/main/java/com/synthese/api/StudentController.java
+++ synthese/src/main/java/com/synthese/api/StudentController.java
@@ -1,11 +1,16 @@
 package com.synthese.api;
 
+import com.synthese.dto.StudentDTO;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("/api/")
+@RequestMapping("/api/student")
 public class StudentController {
+    @GetMapping("/login")
+    public ResponseEntity<StudentDTO> login(@RequestBody StudentDTO studentDTO) {
+        return null;
+    }
 }
