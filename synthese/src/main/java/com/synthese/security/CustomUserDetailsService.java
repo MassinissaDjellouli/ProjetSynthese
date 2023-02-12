d98f6c1e773690f430930b9ff19acf28e6f10e8f
Massinissa Djellouli <massinissadjellouli@gmail.com>
2023-02-12 14:40:14 -0500

    Changed the UserDetailsConfig to become the CustomUserDetailsService

---
+++ synthese/src/main/java/com/synthese/security/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.synthese.security;
+
+import com.synthese.model.User;
+import com.synthese.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
