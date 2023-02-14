424be547d37bfcb7200f1b6a37d1a99c9a1b9ad0
Massinissa Djellouli <massinissadjellouli@gmail.com>
2023-02-13 22:36:38 -0500

    Added fields in the user for the CustomUserDetails

--- synthese/src/main/java/com/synthese/model/User.java
+++ synthese/src/main/java/com/synthese/model/User.java
@@ -1,15 +1,22 @@
 package com.synthese.model;
 
+import com.synthese.security.Roles;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 import org.springframework.validation.annotation.Validated;
+
+import java.time.LocalDateTime;
 
 @Document(collection = "users")
 @Data
@@ -18,12 +25,27 @@ import org.springframework.validation.annotation.Validated;
 @Builder
 public class User {
     @Id
-    private Long id;
+    private ObjectId id;
 
     @NotBlank
+    @Indexed(unique = true)
     @Size(min = 6, max = 12)
     private String username;
     @NotBlank
     @Size(min = 8)
     private String password;
+
+    @NotNull
+    private Roles role;
+
+    @NotNull
+    private LocalDateTime expirationDate;
+
+    @NotNull
+    private LocalDateTime credentialsExpirationDate;
+
+    @NotNull
+    private boolean locked;
+
+
 }
