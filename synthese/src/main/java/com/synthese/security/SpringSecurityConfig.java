0f7b480d372a9d0d7c9836c84ad001cb81159d15
Massinissa Djellouli <massinissadjellouli@gmail.com>
2023-02-13 22:38:33 -0500

    Added the authenticationManager in the SpringSecurityConfig

--- synthese/src/main/java/com/synthese/security/SpringSecurityConfig.java
+++ synthese/src/main/java/com/synthese/security/SpringSecurityConfig.java
@@ -1,7 +1,13 @@
 package com.synthese.security;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,15 +17,19 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @EnableMethodSecurity(securedEnabled = true,jsr250Enabled = true)
 @EnableWebSecurity
+@Configuration
+@AllArgsConstructor
 public class SpringSecurityConfig {
+    private DaoAuthenticationProvider authenticationProvider;
+    private CustomUserDetailsService userDetailsService;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf().disable().authorizeHttpRequests()
                 .requestMatchers(HttpMethod.DELETE).hasRole("ADMIN")
-                .requestMatchers("/api/student/**").hasRole("STUDENT")
-                .requestMatchers("/api/teacher/**").hasRole("TEACHER")
-                .requestMatchers("/api/admin/**").hasRole("ADMIN")
-                .requestMatchers("/api/manager/**").hasRole("MANAGER")
+                .requestMatchers("/api/student/**").hasRole(Roles.STUDENT.name())
+                .requestMatchers("/api/teacher/**").hasRole(Roles.TEACHER.name())
+                .requestMatchers("/api/admin/**").hasRole(Roles.ADMIN.name())
+                .requestMatchers("/api/manager/**").hasRole(Roles.MANAGER.name())
                 .anyRequest().authenticated()
                 .and().httpBasic()
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -27,7 +37,17 @@ public class SpringSecurityConfig {
     }
 
     @Bean
-    BCryptPasswordEncoder passwordEncoder() {
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.authenticationProvider(authenticationProvider);
+        authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(getPasswordEncoder());
+        return authenticationManagerBuilder.build();
+    }
+    @Bean
+    public static BCryptPasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+
+
 }
