383489a7cb9c7a5172996f3ae5218e6acdfa9262
Massinissa Djellouli <massinissadjellouli@gmail.com>
2023-02-13 22:37:13 -0500

    Added Roles Enum

---
+++ synthese/src/main/java/com/synthese/security/Roles.java
@@ -0,0 +1,13 @@
+package com.synthese.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+public enum Roles {
+    ADMIN,
+    STUDENT,
+    TEACHER,
+    MANAGER;
+
+
+
+}
