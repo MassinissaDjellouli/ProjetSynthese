fc81803ef2d945113f33e477f321e80e7abc210a
Massinissa Djellouli <massinissadjellouli@gmail.com>
2023-02-13 22:36:16 -0500

    Replaceed long by ObjectId for the id in the user repository

--- synthese/src/main/java/com/synthese/repository/UserRepository.java
+++ synthese/src/main/java/com/synthese/repository/UserRepository.java
@@ -2,10 +2,13 @@ package com.synthese.repository;
 
 import com.synthese.model.Administrator;
 import com.synthese.model.User;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface UserRepository extends MongoRepository<User, Long> {
-    User findByUsername(String username);
+public interface UserRepository extends MongoRepository<User, ObjectId> {
+    Optional<User> findByUsername(String username);
 }
