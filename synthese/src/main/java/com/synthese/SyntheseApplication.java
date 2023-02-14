090b24dac57ba958ebcb7ff7683e05078e337035
Massinissa Djellouli <massinissadjellouli@gmail.com>
2023-02-13 22:37:27 -0500

    Created bean to add DaoAuthProvider

--- synthese/src/main/java/com/synthese/SyntheseApplication.java
+++ synthese/src/main/java/com/synthese/SyntheseApplication.java
@@ -1,12 +1,24 @@
 package com.synthese;
 
+import com.synthese.security.CustomUserDetailsService;
+import com.synthese.security.SpringSecurityConfig;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 
 @SpringBootApplication
 @AllArgsConstructor
 public class SyntheseApplication {
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(@Lazy CustomUserDetailsService userDetailsService) {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(SpringSecurityConfig.getPasswordEncoder());
+        return authenticationProvider;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SyntheseApplication.class, args);
