4011efa6ab62ade2a2629113212a14a5e59762ac
Massinissa Djellouli <massinissadjellouli@gmail.com>
2023-02-14 20:36:41 -0500

    Added findByUsername in AdministratorRepository

--- synthese/src/main/java/com/synthese/repository/AdministratorRepository.java
+++ synthese/src/main/java/com/synthese/repository/AdministratorRepository.java
@@ -4,6 +4,9 @@ import com.synthese.model.Administrator;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AdministratorRepository extends MongoRepository<Administrator, Long> {
+    Optional<Administrator> findByUsername(String username);
 }
