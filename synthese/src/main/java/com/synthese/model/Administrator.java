c0dc2b13ad4b7701ebcd91c2fdcc0e5280684394
Massinissa Djellouli <massinissadjellouli@gmail.com>
2023-02-14 20:36:04 -0500

    Added fields in Administrator

--- synthese/src/main/java/com/synthese/model/Administrator.java
+++ synthese/src/main/java/com/synthese/model/Administrator.java
@@ -1,4 +1,23 @@
 package com.synthese.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "administrators")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Administrator {
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private LocalDateTime lastConnection;
+
 }
