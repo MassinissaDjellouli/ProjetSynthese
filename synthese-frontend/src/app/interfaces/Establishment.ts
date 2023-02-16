0fb94c1019c7d363505d82bdee8301a0379ddfa8
Massinissa Djellouli <massinissadjellouli@gmail.com>
2023-02-16 14:20:11 -0500

    Added and populated establishment interface

---
+++ synthese-frontend/src/app/interfaces/Establishment.ts
@@ -0,0 +1,24 @@
+import { Program } from "./Program";
+import { Student } from "./Student";
+import { Teacher } from "./Teacher";
+
+export interface Establishment {
+    id: number;
+    name: string;
+    address: string;
+    phone: string;
+    programs: Program[];
+    sessionLength: number;
+    teachers: Teacher[];
+    managers: Manager[];
+    students: Student[];
+    periodLength: number;
+    periodsPerDay: number;
+    daysPerWeek: string[];
+    openTime: string;
+    closeTime: string;
+    classesStartTime: string;
+    periodsBeforeDinner:number;
+    dinnerLength: number;
+    betweenPeriodsLength: number;
+}
