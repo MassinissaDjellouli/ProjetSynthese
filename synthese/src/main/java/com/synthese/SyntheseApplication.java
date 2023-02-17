78f7551ee11f0417b6505d1dce85e5c6e2384bb1
Massinissa Djellouli <massinissadjellouli@gmail.com>
2023-02-16 23:02:25 -0500

    added scanBasePackage

--- synthese/src/main/java/com/synthese/SyntheseApplication.java
+++ synthese/src/main/java/com/synthese/SyntheseApplication.java
@@ -1,28 +1,22 @@
 package com.synthese;
 
-import com.synthese.security.CustomUserDetailsService;
-import com.synthese.security.SpringSecurityConfig;
 import com.synthese.service.AdministratorService;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.synthese", "com.synthese.controller", "com.synthese.dto", "com.synthese.model", "com.synthese.repository", "com.synthese.service"})
 @AllArgsConstructor
 public class SyntheseApplication implements CommandLineRunner {
 
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider(@Lazy CustomUserDetailsService userDetailsService) {
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(userDetailsService);
-        authenticationProvider.setPasswordEncoder(SpringSecurityConfig.getPasswordEncoder());
-        return authenticationProvider;
-    }
-    private AdministratorService adminService;
+    private final AdministratorService adminService;
+
+//    @Bean
+//    public BCryptPasswordEncoder getPasswordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(SyntheseApplication.class, args);
