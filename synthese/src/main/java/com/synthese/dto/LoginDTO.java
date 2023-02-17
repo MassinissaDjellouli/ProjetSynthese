d168ab161c7e37d8b7ae5413b76d09f23b0163a4
Massinissa Djellouli <massinissadjellouli@gmail.com>
2023-02-16 23:11:00 -0500

    added validations in login dto

--- synthese/src/main/java/com/synthese/dto/LoginDTO.java
+++ synthese/src/main/java/com/synthese/dto/LoginDTO.java
@@ -1,5 +1,7 @@
 package com.synthese.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +10,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class LoginDTO {
+    @Size(min = 6, max = 12)
+    @NotBlank
     private String username;
+    @NotBlank
+    @Size(min = 8)
     private String password;
 
 }
