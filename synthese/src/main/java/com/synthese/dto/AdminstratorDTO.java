4a4668b84ca81baacec54d14d62ad5b0598c1891
Massinissa Djellouli <massinissadjellouli@gmail.com>
2023-02-16 23:10:16 -0500

    Added admin dto

---
+++ synthese/src/main/java/com/synthese/dto/AdminstratorDTO.java
@@ -0,0 +1,14 @@
+package com.synthese.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class AdminstratorDTO {
+    private String firstName;
+    private String lastName;
+    private String username;
+}
