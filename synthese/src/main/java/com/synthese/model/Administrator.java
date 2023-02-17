b75ed3ab235be658c7990382beca45ce26bacfca
Massinissa Djellouli <massinissadjellouli@gmail.com>
2023-02-16 23:10:25 -0500

    modified admin to add missing fields

--- synthese/src/main/java/com/synthese/model/Administrator.java
+++ synthese/src/main/java/com/synthese/model/Administrator.java
@@ -1,9 +1,11 @@
 package com.synthese.model;
 
+import com.synthese.dto.AdminstratorDTO;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
@@ -14,10 +16,18 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Builder
 public class Administrator {
+    private ObjectId id;
+    private ObjectId userId;
     private String username;
-    private String password;
     private String firstName;
     private String lastName;
     private LocalDateTime lastConnection;
 
+    public AdminstratorDTO toDTO() {
+        return AdminstratorDTO.builder()
+                .firstName(firstName)
+                .lastName(lastName)
+                .username(username)
+                .build();
+    }
 }
