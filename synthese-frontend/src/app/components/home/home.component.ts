56d72d94286db557e9ef920a4c473deb836174ce
Massinissa Djellouli <massinissadjellouli@gmail.com>
2023-02-17 00:21:53 -0500

    displaying the right home according to the logged in user

--- synthese-frontend/src/app/components/home/home.component.ts
+++ synthese-frontend/src/app/components/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { LoginService } from '../../services/login/login.service';
 import { LoginType } from '../../interfaces/LoginType';
+import { LoggedInService } from '../../services/login/loggedIn/logged-in.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -9,10 +10,20 @@ import { LoginType } from '../../interfaces/LoginType';
 })
 export class HomeComponent {
 
-  constructor(private loginServ:LoginService){
+  constructor(private loginServ:LoginService,private loggedInService:LoggedInService){
 
   }
 
+  isLoggedIn = () => {
+    return this.loggedInService.currentLoggedInUser != undefined;
+  }
+
+  currentUserRole = () => {
+    if(this.loggedInService.currentLoggedInUser == undefined){
+      return undefined;
+    }
+    return this.loggedInService.currentLoggedInUser.role;
+  }
   changeActiveLogin = (item:MenuItem) => {
     this.loginServ.changeLoginType(item.label as LoginType)
   }
