5db42ce11146f66796e3885c10a445e10b54b39d
Massinissa Djellouli <massinissadjellouli@gmail.com>
2023-02-16 23:10:03 -0500

    created establishment object

---
+++ synthese/src/main/java/com/synthese/model/Establishment.java
@@ -0,0 +1,13 @@
+package com.synthese.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Builder
+@AllArgsConstructor
+@Document(collection = "establishments")
+public class Establishment {
+}
