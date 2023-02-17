3a65ef174acca127e2d8a3259b55fb9be3ff2952
Massinissa Djellouli <massinissadjellouli@gmail.com>
2023-02-16 23:08:54 -0500

    added login endpoint in admin controller

---
+++ synthese/src/main/java/com/synthese/controller/AdministratorController.java
@@ -0,0 +1,37 @@
+package com.synthese.controller;
+
+import com.synthese.dto.AdminstratorDTO;
+import com.synthese.dto.LoginDTO;
+import com.synthese.exceptions.AdminNotFoundException;
+import com.synthese.exceptions.UserNotFoundException;
+import com.synthese.exceptions.WrongPasswordException;
+import com.synthese.service.AdministratorService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/admin")
+@CrossOrigin
+public class AdministratorController {
+    AdministratorService adminService;
+
+    @PostMapping("/login")
+    public ResponseEntity<AdminstratorDTO> login(@Valid @RequestBody LoginDTO loginDTO) {
+        try {
+            return ResponseEntity.ok().body(adminService.login(loginDTO).toDTO());
+        } catch (UserNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        } catch (AdminNotFoundException e) {
+            return ResponseEntity.unprocessableEntity().build();
+        } catch (WrongPasswordException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
+
+
+
+}
