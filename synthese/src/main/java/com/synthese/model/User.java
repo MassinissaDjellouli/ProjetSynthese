49133cd73db5d73b9b1014a5c96472e2304eb572
Massinissa Djellouli <massinissadjellouli@gmail.com>
2023-02-16 23:07:18 -0500

    making usernames not unique in user db

--- synthese/src/main/java/com/synthese/model/User.java
+++ synthese/src/main/java/com/synthese/model/User.java
@@ -1,7 +1,6 @@
 package com.synthese.model;
 
 import com.synthese.security.Roles;
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -11,10 +10,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Field;
-import org.springframework.validation.annotation.Validated;
 
 import java.time.LocalDateTime;
 
@@ -28,7 +24,6 @@ public class User {
     private ObjectId id;
 
     @NotBlank
-    @Indexed(unique = true)
     @Size(min = 6, max = 12)
     private String username;
     @NotBlank
