58110e3d26e76e41c7004036843047d1502b0972
Massinissa Djellouli <massinissadjellouli@gmail.com>
2023-02-16 23:06:59 -0500

    removed spring import

--- synthese/src/main/java/com/synthese/security/Roles.java
+++ synthese/src/main/java/com/synthese/security/Roles.java
@@ -1,13 +1,12 @@
 package com.synthese.security;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
 public enum Roles {
     ADMIN,
     STUDENT,
     TEACHER,
     MANAGER;
-
 
 
 }
