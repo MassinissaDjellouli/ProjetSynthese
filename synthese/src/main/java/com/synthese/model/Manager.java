4d7e54778a0afbf3de1d7b42fbb2c4ecfa30152d
Massinissa Djellouli <massinissadjellouli@gmail.com>
2023-02-19 12:44:05 -0500

    added manager annotations

--- synthese/src/main/java/com/synthese/model/Manager.java
+++ synthese/src/main/java/com/synthese/model/Manager.java
@@ -1,8 +1,18 @@
 package com.synthese.model;
 
 import com.synthese.dto.ManagerDTO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Document(collection = "managers")
 public class Manager {
     private ObjectId id;
     private ObjectId userId;
@@ -12,6 +22,7 @@ public class Manager {
 
     public ManagerDTO toDTO() {
         return ManagerDTO.builder()
+                .id(id.toString())
                 .firstName(firstName)
                 .lastName(lastName)
                 .username(username)
--- synthese/src/test/java/com/synthese/controller/TeacherControllerTest.java
+++ synthese/src/test/java/com/synthese/controller/TeacherControllerTest.java
@@ -1,13 +1,99 @@
 package com.synthese.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.synthese.dto.LoginDTO;
+import com.synthese.exceptions.TeacherNotFoundException;
+import com.synthese.exceptions.UserNotFoundException;
+import com.synthese.exceptions.WrongPasswordException;
+import com.synthese.model.Teacher;
+import com.synthese.service.TeacherService;
+import org.bson.types.ObjectId;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
 public class TeacherControllerTest {
-    MockMvc mockMvc;
+    private MockMvc mockMvc;
     @InjectMocks
-    TeacherController teacherController;
+    private TeacherController teacherController;
+    @Mock
+    private TeacherService teacherService;
+
+    private LoginDTO loginDTO;
+    private Teacher teacher;
+
+    private JacksonTester<LoginDTO> loginDTOJacksonTester;
+    private JacksonTester<Teacher> teacherJacksonTester;
+
+    @BeforeEach
+    public void setup() {
+        loginDTO = LoginDTO.builder()
+                .username("teacher123")
+                .password("teacher123")
+                .build();
+        teacher = Teacher.builder()
+                .id(new ObjectId("5f9f1b9b9c9d1b2b8c1c1c1c"))
+                .username("teacher123")
+                .firstName("teacher")
+                .lastName("teacher")
+                .build();
+
+        JacksonTester.initFields(this, new ObjectMapper());
+
+        mockMvc = MockMvcBuilders.standaloneSetup(teacherController).build();
+    }
+
+    @Test
+    public void loginTestHappyDay() throws Exception {
+        when(teacherService.login(any())).thenReturn(teacher);
+
+        mockMvc.perform(post("/api/teacher/login")
+                        .contentType("application/json")
+                        .content(loginDTOJacksonTester.write(loginDTO).getJson()))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void loginTest404() throws Exception {
+        when(teacherService.login(any())).thenThrow(UserNotFoundException.class);
+
+        mockMvc.perform(post("/api/teacher/login")
+                        .contentType("application/json")
+                        .content(loginDTOJacksonTester.write(loginDTO).getJson()))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void loginTest422() throws Exception {
+        when(teacherService.login(any())).thenThrow(TeacherNotFoundException.class);
+
+        mockMvc.perform(post("/api/teacher/login")
+                        .contentType("application/json")
+                        .content(loginDTOJacksonTester.write(loginDTO).getJson()))
+                .andExpect(status().isUnprocessableEntity());
+    }
+
+    @Test
+    public void loginTest401() throws Exception {
+        when(teacherService.login(any())).thenThrow(WrongPasswordException.class);
+
+        mockMvc.perform(post("/api/teacher/login")
+                        .contentType("application/json")
+                        .content(loginDTOJacksonTester.write(loginDTO).getJson()))
+                .andExpect(status().isUnauthorized());
+    }
+
+
 }
