56589207794312d8ef0ff955133bcb2716b01ad9
Massinissa Djellouli <massinissadjellouli@gmail.com>
2023-02-19 12:19:47 -0500

    Added builder in loginDTO

--- synthese/src/main/java/com/synthese/dto/LoginDTO.java
+++ synthese/src/main/java/com/synthese/dto/LoginDTO.java
@@ -3,12 +3,14 @@ package com.synthese.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Builder
 public class LoginDTO {
     @Size(min = 6, max = 12)
     @NotBlank
