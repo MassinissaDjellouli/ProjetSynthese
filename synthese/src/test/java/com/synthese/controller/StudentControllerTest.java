e20423f5f30c889efcadcbc956cd371ee786075f
Massinissa Djellouli <massinissadjellouli@gmail.com>
2023-02-19 12:44:38 -0500

    added student controller tests

--- synthese/src/test/java/com/synthese/controller/StudentControllerTest.java
+++ synthese/src/test/java/com/synthese/controller/StudentControllerTest.java
@@ -1,13 +1,93 @@
 package com.synthese.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.synthese.dto.LoginDTO;
+import com.synthese.exceptions.StudentNotFoundException;
+import com.synthese.exceptions.UserNotFoundException;
+import com.synthese.exceptions.WrongPasswordException;
+import com.synthese.model.Student;
+import com.synthese.service.StudentService;
+import org.bson.types.ObjectId;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
 public class StudentControllerTest {
-    MockMvc mockMvc;
+    private MockMvc mockMvc;
     @InjectMocks
-    StudentController studentController;
+    private StudentController studentController;
+    @Mock
+    private StudentService studentService;
+
+    private LoginDTO loginDTO;
+    private Student student;
+
+    private JacksonTester<LoginDTO> loginDTOJacksonTester;
+    private JacksonTester<Student> studentJacksonTester;
+
+    @BeforeEach
+    public void setup() {
+        loginDTO = LoginDTO.builder()
+                .username("student123")
+                .password("student123")
+                .build();
+        student = Student.builder()
+                .id(new ObjectId("5f9f1b9b9c9d1b2b8c1c1c1c"))
+                .username("student123")
+                .firstName("student")
+                .lastName("student")
+                .build();
+
+        JacksonTester.initFields(this, new ObjectMapper());
+        mockMvc = MockMvcBuilders.standaloneSetup(studentController).build();
+    }
+
+    @Test
+    public void loginTestHappyDay() throws Exception {
+        when(studentService.login(any())).thenReturn(student);
+        mockMvc.perform(post("/api/student/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(loginDTOJacksonTester.write(loginDTO).getJson()))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void loginTest404() throws Exception {
+        when(studentService.login(any())).thenThrow(UserNotFoundException.class);
+        mockMvc.perform(post("/api/student/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(loginDTOJacksonTester.write(loginDTO).getJson()))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void loginTest422() throws Exception {
+        when(studentService.login(any())).thenThrow(StudentNotFoundException.class);
+        mockMvc.perform(post("/api/student/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(loginDTOJacksonTester.write(loginDTO).getJson()))
+                .andExpect(status().isUnprocessableEntity());
+    }
+
+    @Test
+    public void loginTest401() throws Exception {
+        when(studentService.login(any())).thenThrow(WrongPasswordException.class);
+        mockMvc.perform(post("/api/student/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(loginDTOJacksonTester.write(loginDTO).getJson()))
+                .andExpect(status().isUnauthorized());
+    }
 }
