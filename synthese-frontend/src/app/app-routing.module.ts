10e6a5eaff353def29f794244eee91ef06cb809c
Massinissa Djellouli <massinissadjellouli@gmail.com>
2023-02-20 09:22:52 -0500

    Added new pages in router

--- synthese-frontend/src/app/app-routing.module.ts
+++ synthese-frontend/src/app/app-routing.module.ts
@@ -3,11 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AdminComponent } from './components/admin/admin.component';
 import { ConfigureComponent } from './components/admin/configure/configure.component';
+import { EstablishmentComponent } from './components/admin/establishment/establishment.component';
+import { CreateAccountComponent } from './components/admin/establishment/create-account/create-account.component';
 
 const routes: Routes = [
-  // { path: '', component: AdminComponent },
+  //everyone routes
+  { path: '', component: HomeComponent },
+  //admin routes
   { path: 'configure', component: ConfigureComponent },
-   { path: '', component: HomeComponent },
+  { path: 'establishment/:id', component: EstablishmentComponent },
+  { path: 'establishment/:id/createAccount/:type', component: CreateAccountComponent },
+  //student routes
+  //teacher routes
+  //manager routes
 ];
 
 @NgModule({
