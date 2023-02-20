8b330624834524f31d1965907b422f4d8e0b2e6d
Massinissa Djellouli <massinissadjellouli@gmail.com>
2023-02-20 09:11:35 -0500

    Added an error enum in the frontend

---
+++ synthese-frontend/src/app/interfaces/ErrorsEnum.ts
@@ -0,0 +1,9 @@
+export enum Errors{
+    STUDENT_NOT_FOUND = "Aucun étudiant trouvé avec ce matricule",
+    TEACHER_NOT_FOUND = "Aucun professeur trouvé avec ce matricule",
+    MANAGER_NOT_FOUND = "Aucun gestionnaire trouvé avec ce matricule",
+    ADMIN_NOT_FOUND = "Aucun administrateur trouvé avec ce matricule",
+    WRONG_PASSWORD = "Mot de passe invalide",
+    NO_ESTABLISHMENTS = "Aucun établissement trouvé",
+    INVALID_ESTABLISHMENT = "Établissement invalide",
+}
