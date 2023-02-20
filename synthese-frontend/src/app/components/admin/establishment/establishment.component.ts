49602c1765e09ebd30a5a2543018983cf26f2244
Massinissa Djellouli <massinissadjellouli@gmail.com>
2023-02-20 09:12:39 -0500

    Created establishment page to manage an establishment

---
+++ synthese-frontend/src/app/components/admin/establishment/establishment.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { EstablishmentService } from '../../../services/establishment/establishment.service';
+import { Establishment } from '../../../interfaces/Establishment';
+import { NavigationEnd, NavigationStart, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-establishment',
+  templateUrl: './establishment.component.html',
+  styleUrls: ['./establishment.component.css']
+})
+export class EstablishmentComponent {
+
+}
