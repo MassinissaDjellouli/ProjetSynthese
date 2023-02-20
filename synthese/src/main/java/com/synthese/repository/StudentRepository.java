b3277353ee037c454ea9cff012332d35ab8edc03
Massinissa Djellouli <massinissadjellouli@gmail.com>
2023-02-20 11:53:07 -0500

    Added findBy functions in studentRepo

--- synthese/src/main/java/com/synthese/repository/StudentRepository.java
+++ synthese/src/main/java/com/synthese/repository/StudentRepository.java
@@ -6,8 +6,13 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface StudentRepository extends MongoRepository<Student, ObjectId> {
     List<Student> findByUserId(ObjectId id);
+
+    Optional<Student> findByUserIdAndEstablishment(ObjectId id, ObjectId establishment);
+
+    List<Student> findByFirstNameAndLastName(String firstName, String lastName);
 }
