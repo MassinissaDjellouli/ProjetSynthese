e4cc56d0775dd1e98317a58a8792c8f6c74ebde2
Massinissa Djellouli <massinissadjellouli@gmail.com>
2023-02-20 09:18:30 -0500

    Login function for student returns a list of all "students" for a user. ex: 1 student is in 2 establishment, so 2 students will be sent.

--- synthese/src/main/java/com/synthese/repository/StudentRepository.java
+++ synthese/src/main/java/com/synthese/repository/StudentRepository.java
@@ -5,9 +5,9 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface StudentRepository extends MongoRepository<Student, ObjectId> {
-    Optional<Student> findByUserId(ObjectId id);
+    List<Student> findByUserId(ObjectId id);
 }
--- synthese/src/main/java/com/synthese/service/StudentService.java
+++ synthese/src/main/java/com/synthese/service/StudentService.java
@@ -1,6 +1,7 @@
 package com.synthese.service;
 
 import com.synthese.dto.LoginDTO;
+import com.synthese.enums.Roles;
 import com.synthese.exceptions.StudentNotFoundException;
 import com.synthese.exceptions.UserNotFoundException;
 import com.synthese.exceptions.WrongPasswordException;
@@ -8,10 +9,10 @@ import com.synthese.model.Student;
 import com.synthese.model.User;
 import com.synthese.repository.StudentRepository;
 import com.synthese.repository.UserRepository;
-import com.synthese.security.Roles;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -20,7 +21,7 @@ public class StudentService {
     private final UserRepository userRepository;
     private final StudentRepository studentRepository;
 
-    public Student login(LoginDTO loginDTO) throws UserNotFoundException, StudentNotFoundException, WrongPasswordException {
+    public List<Student> login(LoginDTO loginDTO) throws UserNotFoundException, StudentNotFoundException, WrongPasswordException {
         Optional<User> user = userRepository.findByUsernameAndRole(loginDTO.getUsername(), Roles.STUDENT);
         if (user.isEmpty()) {
             throw new UserNotFoundException();
@@ -28,10 +29,10 @@ public class StudentService {
         if (!user.get().getPassword().equals(loginDTO.getPassword())) {
             throw new WrongPasswordException();
         }
-        Optional<Student> studentOpt = studentRepository.findByUserId(user.get().getId());
-        if (studentOpt.isEmpty()) {
+        List<Student> studentList = studentRepository.findByUserId(user.get().getId());
+        if (studentList.isEmpty()) {
             throw new StudentNotFoundException();
         }
-        return studentOpt.get();
+        return studentList;
     }
 }
