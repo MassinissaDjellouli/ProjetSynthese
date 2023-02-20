fdf0fe041cdcd7000f73ec3d16b15edc8f54cf04
Massinissa Djellouli <massinissadjellouli@gmail.com>
2023-02-20 09:20:39 -0500

    Login functions return the result and the errors are handled

--- synthese-frontend/src/app/services/login/login.service.ts
+++ synthese-frontend/src/app/services/login/login.service.ts
@@ -6,6 +6,8 @@ import { ApiResponse } from '../../interfaces/ApiResponse';
 import { Credentials } from 'src/app/interfaces/Credentials';
 import { LoggedInService } from './loggedIn/logged-in.service';
 import { Roles } from '../../interfaces/Roles';
+import { Errors } from '../../interfaces/ErrorsEnum';
+import { LoadingService } from '../loading/loading.service';
 
 
 @Injectable({
@@ -32,26 +34,41 @@ export class LoginService {
         throw new Error("Non-existant user type:" + this.loginType);
     }
   }
-  changeLoginType = (newType:LoginType) => { this.loginType = newType }   
-  login = async (credentials:Credentials):Promise<boolean> => {
-    let loginFunc = this.getLoginFunction();
-    let result:ApiError|ApiResponse = await loginFunc(credentials);
-    if(isError(result)){
-      this.error = parseError(result);
+  changeLoginType = (newType: LoginType) => { this.loginType = newType }
+  login = async (credentials: Credentials): Promise<boolean> => {
+    this.loadingService.startLoading();
+    let result = await this.doLogin(credentials);
+    this.loadingService.stopLoading();
+    return result;
+  }
+
+  private doLogin = async (credentials: Credentials) => {
+    try {
+
+      let loginFunc = this.getLoginFunction();
+      let result: ApiError | ApiResponse = await loginFunc(credentials);
+      if (isError(result)) {
+        this.error = parseError(result);
+        console.log(this.error);
+        return false;
+      }
+      let response = result as ApiResponse;
+      this.loggedInService.login({
+        username: credentials.username,
+        password: credentials.password,
+        userInfo: response.data,
+        lastLoginTime: new Date(),
+        role: this.getRole()
+      });
+      this.error = "";
+      return true;
+    } catch {
+      this.error = "Unknown error";
       return false;
     }
-    let response = result as ApiResponse;
-    this.loggedInService.login({
-      username:credentials.username,
-      password:credentials.password,
-      userInfo:response.data,
-      lastLoginTime:new Date(),
-      role:this.getRole()
-  });
-    return true;
   }
-  private getLoginFunction = ():Function => {
-    switch(this.loginType) {
+  private getLoginFunction = (): Function => {
+    switch (this.loginType) {
       case "Étudiant":
         return this.studentLogin;
       case "Professeur":
@@ -65,21 +82,23 @@ export class LoginService {
     }
   }
 
-  private managerLogin = async (credentials:Credentials) => {
-    this.reqService.postRequest<Credentials>("manager/login",credentials)
-  }
-
-  private adminLogin = async (credentials:Credentials) => {
-    let res = await this.reqService.postRequest<Credentials>("admin/login",credentials);
-    console.log(res);
+  private managerLogin = async (credentials: Credentials) => {
+    let res = await this.reqService.postRequest<Credentials>("manager/login", credentials)
     return res
   }
 
-  private studentLogin = async (credentials:Credentials) => {
-    this.reqService.postRequest<Credentials>("student/login",credentials)
+  private adminLogin = async (credentials: Credentials) => {
+    let res = await this.reqService.postRequest<Credentials>("admin/login", credentials);
+    return res
   }
 
-  private teacherLogin = async (credentials:Credentials) => {
-    this.reqService.postRequest<Credentials>("teacher/login",credentials)
+  private studentLogin = async (credentials: Credentials) => {
+    let res = await this.reqService.postRequest<Credentials>("student/login", credentials)
+    return res
+  }
+
+  private teacherLogin = async (credentials: Credentials) => {
+    let res = await this.reqService.postRequest<Credentials>("teacher/login", credentials)
+    return res
   }
 }
