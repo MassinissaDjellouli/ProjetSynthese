c4f2b347975e586762261a2ca79399799cc51b13
Massinissa Djellouli <massinissadjellouli@gmail.com>
2023-02-20 11:52:42 -0500

    Added createStudent function in admin service

--- synthese/src/main/java/com/synthese/service/AdministratorService.java
+++ synthese/src/main/java/com/synthese/service/AdministratorService.java
@@ -1,18 +1,15 @@
 package com.synthese.service;
 
-import com.synthese.dto.EstablishmentCreationDTO;
-import com.synthese.dto.EstablishmentDTO;
-import com.synthese.dto.LoginDTO;
+import com.synthese.dto.*;
 import com.synthese.enums.Roles;
-import com.synthese.exceptions.AdminNotFoundException;
-import com.synthese.exceptions.EstablishmentNotFoundException;
-import com.synthese.exceptions.UserNotFoundException;
-import com.synthese.exceptions.WrongPasswordException;
+import com.synthese.exceptions.*;
 import com.synthese.model.Administrator;
 import com.synthese.model.Establishment;
+import com.synthese.model.Student;
 import com.synthese.model.User;
 import com.synthese.repository.AdministratorRepository;
 import com.synthese.repository.EstablishmentRepository;
+import com.synthese.repository.StudentRepository;
 import com.synthese.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.bson.types.ObjectId;
@@ -29,6 +26,7 @@ public class AdministratorService {
     private final AdministratorRepository adminRepository;
     private final UserRepository userRepository;
     private final EstablishmentRepository establishmentRepository;
+    private final StudentRepository studentRepository;
     private final String DEFAULT_ADMIN_PASSWORD = "12345678";
     private final String DEFAULT_ADMIN_USERNAME = "admin123";
 
@@ -90,5 +88,40 @@ public class AdministratorService {
             throw new EstablishmentNotFoundException();
         }
         return establishmentOptional;
+    }
+
+    public ObjectId createStudent(CreateStudentDTO studentCreationDTO) throws AlreadyExistingStudentException {
+        Optional<User> userOpt = userRepository.findByUsernameAndRole(studentCreationDTO.getUsername(), Roles.STUDENT);
+        if (userOpt.isPresent()) {
+            return createStudentForExistingUser(studentCreationDTO, userOpt.get());
+        }
+        User user = createNewUser(studentCreationDTO.getUsername(), studentCreationDTO.getPassword(), Roles.STUDENT);
+        return createStudentForExistingUser(studentCreationDTO, user);
+    }
+
+    private ObjectId createStudentForExistingUser(CreateStudentDTO studentCreationDTO, User user) throws AlreadyExistingStudentException {
+        Optional<Student> studentOpt = studentRepository.findByUserIdAndEstablishment(
+                user.getId(),
+                new ObjectId(studentCreationDTO.getEstablishmentId()));
+        if (studentOpt.isPresent()) {
+            throw new AlreadyExistingStudentException();
+        }
+        return studentRepository.save(Student.builder()
+                .userId(user.getId())
+                .establishment(new ObjectId(studentCreationDTO.getEstablishmentId()))
+                .firstName(studentCreationDTO.getFirstName())
+                .lastName(studentCreationDTO.getLastName())
+                .build()).getId();
+    }
+
+    public List<StudentDTO> getStudentsByName(String firstName, String lastName) {
+        return studentRepository.findByFirstNameAndLastName(firstName, lastName).stream().map(student -> {
+            User user = userRepository.findById(student.getUserId()).orElseThrow();
+            return StudentDTO.builder()
+                    .firstName(student.getFirstName())
+                    .lastName(student.getLastName())
+                    .username(user.getUsername())
+                    .build();
+        }).toList();
     }
 }
