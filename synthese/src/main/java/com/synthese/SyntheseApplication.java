ce29428d1e67ce8e4fcf25a7b770fb8eb8ff6340
Massinissa Djellouli <massinissadjellouli@gmail.com>
2023-02-23 23:39:45 -0500

    removed scanBasePackages

--- synthese/src/main/java/com/synthese/SyntheseApplication.java
+++ synthese/src/main/java/com/synthese/SyntheseApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@SpringBootApplication(scanBasePackages = {"com.synthese", "com.synthese.controller", "com.synthese.dto", "com.synthese.model", "com.synthese.repository", "com.synthese.service"})
+@SpringBootApplication()
 @AllArgsConstructor
 public class SyntheseApplication implements CommandLineRunner {
 
