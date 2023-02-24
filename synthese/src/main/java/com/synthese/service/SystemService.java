fd19d1b15771008d8cb042686863d5c6e3cad1f7
Massinissa Djellouli <massinissadjellouli@gmail.com>
2023-02-23 23:39:57 -0500

    Added function to get all establishments

--- synthese/src/main/java/com/synthese/service/SystemService.java
+++ synthese/src/main/java/com/synthese/service/SystemService.java
@@ -1,9 +1,24 @@
 package com.synthese.service;
 
+import com.synthese.dto.EstablishmentDTO;
+import com.synthese.repository.EstablishmentRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class SystemService {
+
+    private final EstablishmentRepository establishmentRepository;
+
+    public List<EstablishmentDTO> getEstablishments() {
+        return establishmentRepository.findAll().stream().map(establishment ->
+                EstablishmentDTO.builder()
+                        .id(establishment.getId().toString())
+                        .name(establishment.getName())
+                        .build()
+        ).toList();
+    }
 }
