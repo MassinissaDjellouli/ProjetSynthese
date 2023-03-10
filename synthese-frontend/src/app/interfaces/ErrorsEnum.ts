5de4ae08237dc0987b2ce1c9a07b0c59e7d2d7f7
Massinissa Djellouli <massinissadjellouli@gmail.com>
2023-03-10 11:17:03 -0500

    added more errors in error enums

--- synthese-frontend/src/app/interfaces/ErrorsEnum.ts
+++ synthese-frontend/src/app/interfaces/ErrorsEnum.ts
@@ -6,5 +6,7 @@ export enum Errors{
     UNKNOWN_ERROR = "Erreur inconnue",
     ALREADY_EXISTING_STUDENT = "Cet élève existe déjà sur cet établissement",
     ALREADY_EXISTING_MANAGER = "Ce gestionnaire existe déjà sur cet établissement",
-    ALREADY_EXISTING_TEACHER = "Ce professeur existe déjà sur cet établissement"    
+    ALREADY_EXISTING_TEACHER = "Ce professeur existe déjà sur cet établissement",
+    ALREADY_EXISTING_PROGRAM = "Un de ces programmes existe déjà sur cet établissement",
+    ALREADY_EXISTING_COURSE = "Un de ces cours existe déjà pour ce programme",
 }
