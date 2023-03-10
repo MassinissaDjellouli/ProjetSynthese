b24dec9d5e66e85cd15bbad887ea13256aac5bf6
Massinissa Djellouli <massinissadjellouli@gmail.com>
2023-03-10 11:21:36 -0500

    added list of courses in teacher and dto

--- synthese/src/main/java/com/synthese/dto/TeacherDTO.java
+++ synthese/src/main/java/com/synthese/dto/TeacherDTO.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 @Builder
 @AllArgsConstructor
@@ -13,4 +15,5 @@ public class TeacherDTO {
     private String lastName;
     private String username;
     private String establishmentId;
+    private List<CourseDTO> courses;
 }
--- synthese/src/main/java/com/synthese/model/Teacher.java
+++ synthese/src/main/java/com/synthese/model/Teacher.java
@@ -7,6 +7,8 @@ import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.List;
+
 @Data
 @Builder
 @AllArgsConstructor
@@ -18,14 +20,38 @@ public class Teacher {
     private String lastName;
     private String username;
     private ObjectId establishment;
+    private List<ObjectId> courses;
 
 
-    public TeacherDTO toDTO() {
+    public TeacherDTO toDTO(List<Course> courses) {
         return TeacherDTO.builder()
                 .id(id.toString())
                 .firstName(firstName)
                 .lastName(lastName)
                 .username(username)
+                .courses(courses.stream().map(Course::toDTO).toList())
+                .build();
+    }
+
+    public TeacherDTO toDTO(String establishment, List<Course> courses) {
+        return TeacherDTO.builder()
+                .id(id.toString())
+                .firstName(firstName)
+                .lastName(lastName)
+                .username(username)
+                .establishmentId(establishment)
+                .courses(courses.stream().map(Course::toDTO).toList())
+                .build();
+    }
+
+    public TeacherDTO toDTO(String establishment) {
+        return TeacherDTO.builder()
+                .id(id.toString())
+                .firstName(firstName)
+                .lastName(lastName)
+                .username(username)
+                .establishmentId(establishment)
+                .courses(List.of())
                 .build();
     }
 }
