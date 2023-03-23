b48ea6125ebbbb1cd91555bf8087ffb84240180b
Massinissa Djellouli <massinissadjellouli@gmail.com>
2023-03-22 23:34:15 -0400

    Created DTO to link student to program

---
+++ synthese/src/main/java/com/synthese/dto/StudentLinkDTO.java
@@ -0,0 +1,18 @@
+package com.synthese.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class StudentLinkDTO {
+    @NotBlank
+    @Size(min = 8, max = 12)
+    private String username;
+    @NotBlank
+    private String programName;
+}
