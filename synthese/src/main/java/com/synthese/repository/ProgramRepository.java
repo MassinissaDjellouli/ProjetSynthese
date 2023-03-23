4742246746fe4c7fcb7a44d86561bf54523e7c88
Massinissa Djellouli <massinissadjellouli@gmail.com>
2023-03-22 23:35:59 -0400

    Added function to get program using establishmentname and name of program

--- synthese/src/main/java/com/synthese/repository/ProgramRepository.java
+++ synthese/src/main/java/com/synthese/repository/ProgramRepository.java
@@ -6,9 +6,12 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 
 public interface ProgramRepository extends MongoRepository<Program, ObjectId> {
     List<Program> findByEstablishment(ObjectId establishment);
+
+    Optional<Program> findByEstablishmentAndName(ObjectId establishment, String name);
 }
