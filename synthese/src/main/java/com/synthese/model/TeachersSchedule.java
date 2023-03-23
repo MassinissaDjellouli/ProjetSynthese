a235fdfa1e5b3808b7e1b330d600a4b486315a06
Massinissa Djellouli <massinissadjellouli@gmail.com>
2023-03-22 23:34:55 -0400

    Created TeacherSchedule object

---
+++ synthese/src/main/java/com/synthese/model/TeachersSchedule.java
@@ -0,0 +1,21 @@
+package com.synthese.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Builder
+@Data
+@NoArgsConstructor
+@Document(collection = "teachers_schedule")
+public class TeachersSchedule {
+    private String id;
+    private String teacherId;
+    private String establishmentId;
+    List<CourseScheduleBlock> scheduleBlocks;
+}
