461a9bf7b91da2dd7021cd9e81edbff564db9a36
Massinissa Djellouli <mdjellouli@generixgroup.com>
2023-03-24 11:44:06 -0400

    Added limits on calendar and showing only open days

--- synthese-frontend/src/app/components/teacher/create-schedule/create-schedule.component.ts
+++ synthese-frontend/src/app/components/teacher/create-schedule/create-schedule.component.ts
@@ -15,8 +15,17 @@ import { LoadingService } from '../../../services/loading/loading.service';
 export class CreateScheduleComponent {
   calendarEnabled = false;
   WEEK_DAYS = ['Dimanche','Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'];
-  filteredWeekDays:string[] = [];
-  daysPerWeek = () => {console.log(this.filteredWeekDays.length);this.filteredWeekDays.length}
+  filteredWeekDays:number[] = [];
+  setFilteredWeekDays = (daysPerWeek:string[]) => {
+    let index = 0;
+    for(let i = 0; i < this.WEEK_DAYS.length; i++){
+      if(this.WEEK_DAYS[i] == daysPerWeek[index]){
+        this.filteredWeekDays.push(i);
+        index++;
+      }
+    }
+    this.updateCalendarOptions();
+  }
   
   establishment:Establishment|undefined;
   calendarError:string = "";
@@ -39,8 +48,57 @@ export class CreateScheduleComponent {
         return;
       }
       this.establishment = (res as ApiResponse).data as Establishment;
-      this.calendarEnabled = true;
-      this.filteredWeekDays = this.establishment.daysPerWeek
+      this.setFilteredWeekDays(this.establishment.daysPerWeek);
       this.loadingService.stopLoading();
+    }
+    updateCalendarOptions = () => {
+      this.calendarOptions = {
+        locale:'fr',
+        plugins: [ timeGridPlugin ],
+        nowIndicator:true,
+        slotMinTime:this.establishment!.openTime,
+        slotMaxTime:this.establishment!.closeTime,
+        initialView: 'timeGridWeek',
+        businessHours:{
+          daysOfWeek:this.filteredWeekDays,
+          startTime:this.establishment!.classesStartTime,
+          endTime:this.getEndTime()
+        },
+        hiddenDays:this.getHiddenDays(),
+        dayHeaderContent: (date) => {
+          return this.WEEK_DAYS[date.date.getDay()];
+        }
+      }
+      this.calendarEnabled = true;
+    }
+    getEndTime = () =>{
+      let AMOUNT_OF_PERIODS_WITHOUT_PAUSES = 2
+      let MINUTES_PER_HOURS = 60
+      let endTime:number[] = this.establishment!.classesStartTime.split(":").map(time => Number.parseInt(time));
+      let pauseTime:number = this.establishment!.betweenPeriodsLength;
+      let dinnerTime:number = this.establishment!.dinnerLength;
+      let periodsPerDay:number = this.establishment!.periodsPerDay;
+      let periodLength:number = this.establishment!.periodLength;
+      let daysLength:number = dinnerTime * 1 + (periodLength * periodsPerDay) + (pauseTime * (periodsPerDay - AMOUNT_OF_PERIODS_WITHOUT_PAUSES));
+      let minutes = daysLength % MINUTES_PER_HOURS;
+      let hours = (daysLength - minutes) / MINUTES_PER_HOURS;
+      endTime[0] += hours;
+      endTime[1] += minutes ;
+      if(endTime[1] >= 60){
+        endTime[1] -= 60
+        endTime[0]++
+      }
+      return endTime.map(time => {
+        return time.toString().length == 2? time.toString() : "0" + time.toString()
+      }).join(":");
+    }
+    getHiddenDays = () => {
+      let hiddenDays = [];
+      for(let i = 0; i < this.WEEK_DAYS.length; i++){
+        if(!this.filteredWeekDays.includes(i)){
+          hiddenDays.push(i);
+        }
+      }
+      return hiddenDays
     }
 }
